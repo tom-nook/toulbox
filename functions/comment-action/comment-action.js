fac23e2e588112cf51a6b3992f28e325eacd0670
llc <touldeguiacranmer@gmail.com>
2020-10-01 18:10:54 -0500

    :bug: Added back form notification for approved

--- functions/comment-action/comment-action.js
+++ functions/comment-action/comment-action.js
@@ -8,7 +8,7 @@ const {
 } = process.env;
 
 // hardcoding this for a moment... TODO: replace request with somethign that follows redirects
-const URL = "https://dev.oscato.com/slash-command-testing";
+const URL = "https://thetoulbox.com/";
 
 /*
   delete this submission via the api
